00b35a934a7e3d950916a6b682acbb7061db610b
Arthur Alves Magalhães <arthur.alves@transfacil.com.br>
2024-09-25 14:07:44 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.ModelViews;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => Results.Json(new Home()));
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+    if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login realizado com sucesso");
     else
         return Results.Unauthorized();
 });
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
